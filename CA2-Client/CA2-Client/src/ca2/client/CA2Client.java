4c9bc375aa63879ff2378bb6e38aa50d26e3652e
Mikkel <mikkel@mediagram.dk>
2017-10-02 11:23:50 +0200

    2 Projects

---
+++ CA2-Client/CA2-Client/src/ca2/client/CA2Client.java
@@ -0,0 +1,16 @@
+package ca2.client;
+
+/**
+ *
+ * @author
+ */
+public class CA2Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ CA2-Server/src/ca2/server/Server.java
@@ -0,0 +1,10 @@
+package ca2.server;
+
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
