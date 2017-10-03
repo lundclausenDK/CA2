64c4a479553b32480eb6c110ba9e45079d9ecdb0
Mikkel <mikkel@mediagram.dk>
2017-10-03 23:35:27 +0200

    Documentation and code cleanup

--- CA2-Server/src/ca2/server/Postman.java
+++ CA2-Server/src/ca2/server/Postman.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca2.server;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author craci
- */
 public class Postman extends Thread {
     
     public static ArrayBlockingQueue<String> messages = new ArrayBlockingQueue(30);
@@ -26,10 +15,10 @@ public class Postman extends Thread {
                 switch(toBeSendArray[0]){
                     case "MSG":{
                         String[] receiversAndMessages = toBeSendArray[1].split(":");
-                        String[] recievers = receiversAndMessages[0].split(",");
+                        String[] receivers = receiversAndMessages[0].split(",");
                         for (ClientPocket client : Server.clients) {
-                            for (String receiver : recievers) {
-                                if (recievers[0].equals("*") || client.getChatterName().equals(receiver)) {
+                            for (String receiver : receivers) {
+                                if (receivers[0].equals("*") || client.getChatterName().equals(receiver)) {
                                     client.sendMessage("MSGRES:" + receiversAndMessages[2] + ":" + receiversAndMessages[1]);
                                 }
                             }
