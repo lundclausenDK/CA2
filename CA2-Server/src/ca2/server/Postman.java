bc828e15460b383380725e8d7a0701d285f7614e
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-10-03 15:50:50 +0200

    LALA land works

--- CA2-Server/src/ca2/server/Postman.java
+++ CA2-Server/src/ca2/server/Postman.java
@@ -22,13 +22,36 @@ public class Postman extends Thread {
         while(true){
             try {
                 String toBeSend = messages.take();
-                String[] receiversAndMessages = toBeSend.split(":");
-                String[] recievers = receiversAndMessages[0].split(",");
-                for (ClientPocket client : Server.clients) {
-                    for (String receiver : recievers) {
-                        if(recievers[0].equals("*") || client.getChatterName().equals(receiver)){
-                            client.sendMessage("MSGRES:"+receiversAndMessages[2]+":"+receiversAndMessages[1]);
+                String[] toBeSendArray = toBeSend.split(":", 2);
+                switch(toBeSendArray[0]){
+                    case "MSG":{
+                        String[] receiversAndMessages = toBeSendArray[1].split(":");
+                        String[] recievers = receiversAndMessages[0].split(",");
+                        for (ClientPocket client : Server.clients) {
+                            for (String receiver : recievers) {
+                                if (recievers[0].equals("*") || client.getChatterName().equals(receiver)) {
+                                    client.sendMessage("MSGRES:" + receiversAndMessages[2] + ":" + receiversAndMessages[1]);
+                                }
+                            }
                         }
+                        break;
+                    }
+                    
+                    case "CLIENTLIST":{
+                        String message = "CLIENTLIST:";
+                        for (int i = 0; i < Server.clients.size(); i++) {
+                            if(i != Server.clients.size()-1){
+                                message += Server.clients.get(i).getChatterName()+",";
+                            }else{
+                                message += Server.clients.get(i).getChatterName();
+                            }
+                        }
+                        
+                        for (ClientPocket client : Server.clients) {
+                            client.sendMessage(message);
+                        }
+                        
+                        break;
                     }
                 }
             } catch (InterruptedException ex) {
