d70e76a24cd7a75e28c495fb8ad040090db688ee
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-10-03 14:07:41 +0200

    a bit futher than the beginning aka Postman

---
+++ CA2-Server/src/ca2/server/Postman.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca2.server;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author craci
+ */
+public class Postman extends Thread {
+    
+    public static ArrayBlockingQueue<String> messages = new ArrayBlockingQueue(30);
+    
+    public void run(){
+        while(true){
+            try {
+                String toBeSend = messages.take();
+                String[] receiversAndMessages = toBeSend.split(":");
+                String[] recievers = receiversAndMessages[0].split(",");
+                for (ClientPocket client : Server.clients) {
+                    for (String receiver : recievers) {
+                        if(recievers[0].equals("*") || client.getName().equals(receiver)){
+                            client.sendMessage(receiversAndMessages[1]);
+                        }
+                    }
+                }
+            } catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
--- CA2-Server/src/ca2/server/Server.java
+++ CA2-Server/src/ca2/server/Server.java
@@ -1,22 +1,21 @@
 package ca2.server;
 
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Server {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     ExecutorService es = Executors.newFixedThreadPool(30);
+    public static ClientPocket[] clients;
 
     public void startServer() {
         try {
             serverSocket = new ServerSocket(8081);
+            
 
             System.out.println("server is running");
             while (true) {
@@ -27,8 +26,8 @@ public class Server {
                 
             }
 
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());;
         }
     }
 
