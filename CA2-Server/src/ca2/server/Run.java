36a9c8368f578d38499a3605038903ccfbbc723f
Peter Rasmussen <leonhardt_88@live.dk>
2017-10-03 10:22:26 +0200

    server can recieve clients

---
+++ CA2-Server/src/ca2/server/Run.java
@@ -0,0 +1,10 @@
+
+
+package ca2.server;
+
+
+public class Run {
+    public static void main(String[] args) {
+        
+    }
+}
--- CA2-Server/src/ca2/server/Server.java
+++ CA2-Server/src/ca2/server/Server.java
@@ -1,10 +1,32 @@
 package ca2.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(8081);
+
+            System.out.println("server is running");
+            while (true) {
+                
+                
+                clientSocket = serverSocket.accept();
+                System.out.println("Client recieved");
+                
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
